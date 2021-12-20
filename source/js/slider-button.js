f6a4cb03637f19bc87ac0fa9bba7a14cd03e2d44
Хохлова Наталия <nataliebasket@gmail.com>
2021-12-20 11:04:23 +0300

    catalog retina

--- source/js/slider-button.js
+++ source/js/slider-button.js
@@ -5,16 +5,16 @@ let catAfter = document.querySelector('.example__img-after');
 let middleButton = document.querySelector('.example__button');
 
 buttonAfter.onclick = function () {
-  // catBefore.style.display = "none";
-  // catAfter.style.display = "block";
+  //catBefore.style.display = "none";
+  //catAfter.style.display = "block";
   catBefore.style.opacity = "0";
   catAfter.style.opacity = "1";
   middleButton.style.left = "43px";
 }
 
 buttonBefore.onclick = function () {
-  // catAfter.style.display = "none";
-  // catBefore.style.display = "block";
+  //catAfter.style.display = "none";
+  //catBefore.style.display = "block";
   catAfter.style.opacity = "0";
   catBefore.style.opacity = "1";
   middleButton.style.left = "6px";
