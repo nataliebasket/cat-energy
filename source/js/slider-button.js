5fc103b33dbd35ba1896c83d24236d77b14270ca
Хохлова Наталия <nataliebasket@gmail.com>
2022-01-13 09:49:17 +0300

    Edit marks in slider.js

--- source/js/slider-button.js
+++ source/js/slider-button.js
@@ -1,7 +1,5 @@
 let buttonBefore = document.querySelector(".example__button-before");
 let buttonAfter = document.querySelector(".example__button-after");
-let catBefore = document.querySelector(".example__img-fat ");
-let catAfter = document.querySelector(".example__img-skin");
 let thumb = document.querySelector(".example__button");
 let sizeCurtainLeft = document.querySelector(".example__div-fat");
 let sizeCurtainRight = document.querySelector(".example__div-skin");
@@ -14,7 +12,6 @@ let x1 = Math.round(track.x);
 let x2 = Math.round(track.x + track.width);
 
 let trackWidth;
-
 let version;
 let currentLeftWidth;
 let currentRightWidth;
@@ -24,16 +21,16 @@ version = getComputedStyle(document.documentElement).getPropertyValue("--window-
 
 function windowResize () {
   version = getComputedStyle(document.documentElement).getPropertyValue("--window-version");
-  if (version == "'tablet'") {
+  if (version === '"tablet"') {
     thumb.style.setProperty("--left-position", "50%");
-    sizeCurtainLeft.style.setProperty("--width-curtain-left", `296px`);
-    sizeCurtainRight.style.setProperty("--width-curtain-right", `296px`);
+    sizeCurtainLeft.style.setProperty("--width-curtain-left", "296px");
+    sizeCurtainRight.style.setProperty("--width-curtain-right", "296px");
     //console.log(version);
   }
-  if (version == "'mobile'") {
+  if (version === '"mobile"') {
     thumb.style.setProperty("--left-position", "21px");
-    sizeCurtainLeft.style.setProperty("--width-curtain-left", `280px`);
-    sizeCurtainRight.style.setProperty("--width-curtain-right", `1px`);
+    sizeCurtainLeft.style.setProperty("--width-curtain-left", "280px");
+    sizeCurtainRight.style.setProperty("--width-curtain-right", "1px");
   }
 }
 
@@ -59,7 +56,6 @@ thumb.addEventListener("mousedown", function (e) {
 
   curtainWidth = getComputedStyle(document.querySelector(".example__grid")).getPropertyValue("--curtain-width");
   trackWidth = getComputedStyle(document.querySelector(".example__track")).getPropertyValue("--track-width");
-  //console.log(trackWidth);
 
   document.onmousemove = function (e) {
     moveThumb(e);
@@ -71,61 +67,21 @@ thumb.addEventListener("mousedown", function (e) {
   }
 });
 
-
 buttonAfter.onclick = function () {
-  sizeCurtainLeft.style.setProperty("--width-curtain-left", `0px`);
+  sizeCurtainLeft.style.setProperty("--width-curtain-left", "0px");
 
-  if (version === "'mobile'") {
-    sizeCurtainRight.style.setProperty("--width-curtain-right", `280px`);
+  if (version === '"mobile"') {
+    sizeCurtainRight.style.setProperty("--width-curtain-right", "280px");
   } else {
-    sizeCurtainRight.style.setProperty("--width-curtain-right", `492px`);
+    sizeCurtainRight.style.setProperty("--width-curtain-right", "492px");
   }
 }
 
 buttonBefore.onclick = function () {
-  if (version === "'mobile'") {
-    sizeCurtainLeft.style.setProperty("--width-curtain-left", `280px`);
+  if (version === '"mobile"') {
+    sizeCurtainLeft.style.setProperty("--width-curtain-left", "280px");
   } else {
-    sizeCurtainLeft.style.setProperty("--width-curtain-left", `492px`);
+    sizeCurtainLeft.style.setProperty("--width-curtain-left", "492px");
   }
-
-  sizeCurtainRight.style.setProperty("--width-curtain-right", `0px`);
+  sizeCurtainRight.style.setProperty("--width-curtain-right", "0px");
 }
-
-
-
-
-// if (version === ""mobile"") {
-//   buttonAfter.onclick = function () {
-//     catBefore.style.opacity = "0";
-//     catAfter.style.opacity = "1";
-//     thumb.style.left = "43px";
-//   }
-//   buttonBefore.onclick = function () {
-//     catAfter.style.opacity = "0";
-//     catBefore.style.opacity = "1";
-//     thumb.style.left = "6px";
-//   }
-// }
-
-// if (version === ""tablet"") {
-//   function moveThumb(e) {
-//     thumb.style.setProperty("--left-position", (e.pageX - track.x) + "px");
-//     if (e.pageX > x2) [thumb.style.setProperty("--left-position", track.width + "px")];
-//     if (e.pageX < x1) [thumb.style.setProperty("--left-position", "0px")];
-//     if ((e.pageX < x2) && (e.pageX > x1)) {
-//       sizeCurtainLeft.style.width = Math.round(592 / 426 * (e.pageX - track.x)) + "px";
-//       sizeCurtainRight.style.width = (592 - Math.round(592 / 426 * (e.pageX - track.x))) + "px";
-//     }
-//   }
-
-//   thumb.addEventListener("mousedown", function (e) {
-//     document.onmousemove = function (e) {
-//       moveThumb(e);
-//     }
-//     document.onmouseup = function () {
-//       document.onmousemove = null;
-//       document.onmouseup = null;
-//     }
-//   });
-// };
